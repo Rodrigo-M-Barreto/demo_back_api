f16890819fbfba2e0234f1278f39e27cb80052d4
Rodrigo Barreto <grungeria1408@gmail.com>
2024-04-12 20:07:51 -0300

    adicionando autenticacao e entidade paciente

--- src/main/java/com/example/demo_back_api/controllers/PacienteController.java
+++ src/main/java/com/example/demo_back_api/controllers/PacienteController.java
@@ -16,45 +16,45 @@ import com.example.demo_back_api.repositories.UserRepository;
 @RestController
 public class PacienteController {
 
-	private final PacienteRepository pacienteRepository; 
+	private final PacienteRepository pacienteRepository;
 	private final UserRepository userRepository;
-	
+
 	public PacienteController(PacienteRepository pacienteRepository, UserRepository userRepository) {
 		this.pacienteRepository = pacienteRepository;
 		this.userRepository = userRepository;
 	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Void> createTweet(@RequestBody CreatePacienteDto dto, JwtAuthenticationToken token) {
+		var user = userRepository.findById(UUID.fromString(token.getName()));
+
+		var paciente = new Paciente();
+		paciente.setUser(user.get());
+		paciente.setNomePaciente(dto.nomePaciente());
+
+		pacienteRepository.save(paciente);
+
+		return ResponseEntity.ok().build();
+	}
+
 	
-	 @PostMapping("/cadastrar")
-	    public ResponseEntity<Void> createTweet(@RequestBody CreatePacienteDto dto,
-	                                            JwtAuthenticationToken token) {
-	        var user = userRepository.findById(UUID.fromString(token.getName()));
+	@DeleteMapping("/tweets/{id}")
+	public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId, JwtAuthenticationToken token) {
+		var user = userRepository.findById(UUID.fromString(token.getName()));
+		var tweet = tweetRepository.findById(tweetId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-	        var paciente = new Paciente();
-	        paciente.setUser(user.get());
-	        paciente.setNomePaciente(dto.nomePaciente());
+		var isAdmin = user.get().getRoles().stream()
+				.anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
 
-	        pacienteRepository.save(paciente);
+		if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+			tweetRepository.deleteById(tweetId);
 
-	        return ResponseEntity.ok().build();
-	    }
+		} else {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		}
 
-		/*
-		 * @DeleteMapping("/tweets/{id}") public ResponseEntity<Void>
-		 * deleteTweet(@PathVariable("id") Long tweetId, JwtAuthenticationToken token) {
-		 * var user = userRepository.findById(UUID.fromString(token.getName())); var
-		 * tweet = tweetRepository.findById(tweetId) .orElseThrow(() -> new
-		 * ResponseStatusException(HttpStatus.NOT_FOUND));
-		 * 
-		 * var isAdmin = user.get().getRoles() .stream() .anyMatch(role ->
-		 * role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
-		 * 
-		 * if (isAdmin ||
-		 * tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
-		 * tweetRepository.deleteById(tweetId);
-		 * 
-		 * } else { return ResponseEntity.status(HttpStatus.FORBIDDEN).build(); }
-		 * 
-		 * 
-		 * return ResponseEntity.ok().build(); }
-		 */
+		return ResponseEntity.ok().build();
+	}
+	 
 }
